43e14fea0ca2b87202095fa767fd68d2a5a0cf47
Dheeraj Manjunath <dheerajmanju1@gmail.com>
2023-05-29 16:09:41 -0400

    Add clap to do argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,48 @@
-use std::fmt::format;
-
+use clap::Parser;
 use reqwest::{self, StatusCode};
 use tokio;
 
+#[derive(Parser, Default, Debug)]
+#[clap(author = "dmanjunath", version = "0.0.1", about = "eip-lookup")]
+struct Arguments {
+    eip_id: String,
+
+    #[arg(short, long)]
+    browser: bool,
+}
 #[tokio::main]
 async fn main() {
-    let args: Vec<String> = std::env::args().collect();
-    if args.len() < 2 {
-        eprintln!("Please provide an eip_id. Usage `eip-lookup <eip_id>`");
-        std::process::exit(1);
-    }
-    let eip_id = &args[1];
-
-    // println!(" outputs {eip_id} ");
+    let args = parse_clap_args();
+    let eip_id = args.eip_id;
+    let browser = args.browser;
 
     let raw_url = make_raw_url(&eip_id);
-    match make_req(&raw_url).await {
-        Ok(()) => (),
+
+    match check_eip_exists(&raw_url).await {
+        Ok(true) => println!("EIP {} exists", eip_id),
+        Ok(false) => {
+            eprintln!("EIP {} does not exist", eip_id);
+            std::process::exit(1);
+        }
         Err(e) => println!("Could not make a request because of {e}"),
     }
 
     // open url in browser
-    let github_url = make_github_url(&eip_id);
-    match open::that(&github_url) {
-        Ok(()) => println!("Opened '{}' successfully.", &github_url),
-        Err(err) => panic!("An error occurred when opening '{}': {}", &github_url, err),
+    if browser {
+        let github_url = make_github_url(&eip_id);
+        open_in_browser(&github_url);
+    } else {
+        match fetch_body(&raw_url).await {
+            Ok((body, _)) => println!("{body}", body = body),
+            Err(e) => println!("Could not make a request because of {e}"),
+        }
     }
+}
+
+fn parse_clap_args() -> Arguments {
+    let args = Arguments::parse();
+    // println!("Clap arguments: {:?}", args);
+    args
 }
 
 fn make_raw_url(eip_id: &str) -> String {
@@ -44,7 +61,27 @@ fn make_github_url(eip_id: &str) -> String {
     url
 }
 
-async fn make_req(url: &String) -> Result<(), reqwest::Error> {
+fn open_in_browser(url: &String) {
+    match open::that(url) {
+        Ok(()) => println!("Opened '{}' successfully.", url),
+        Err(err) => panic!("An error occurred when opening '{}': {}", url, err),
+    }
+}
+
+async fn check_eip_exists(url: &String) -> Result<bool, reqwest::Error> {
+    let client = reqwest::Client::new();
+    let response = client.head(url).send().await?;
+
+    // get status code from response
+    let status = response.status();
+    if status == StatusCode::from_u16(200).unwrap() {
+        return Ok(true);
+    } else {
+        return Ok(false);
+    }
+}
+
+async fn fetch_body(url: &String) -> Result<(String, StatusCode), reqwest::Error> {
     let response = reqwest::get(url).await?;
     // get status code from response
     let status = response.status();
@@ -59,7 +96,5 @@ async fn make_req(url: &String) -> Result<(), reqwest::Error> {
     // get body from response
     let body = response.text().await?;
 
-    println!("{body}");
-
-    Ok(())
+    Ok((body, status))
 }
